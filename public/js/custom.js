64063c58f7aeeb34b2d24cb5a8960a80c79768b8
qcer1 <1421991449@qq.com>
2017-03-19 13:10:15 +0800

    add the PV

--- public/js/custom.js
+++ public/js/custom.js
@@ -11,4 +11,36 @@ $(function () {
        $('html, body').animate({scrollTop: 0}, 500);
    });
 
+
+   // show the page visit
+   showPV();
+   function showPV() {
+      // body...
+      var pvcountObj = {pvcount:null};
+      var url = "";
+      if (window.location.pathname == "/blog/index.html"){
+         url = "http://cloud.bmob.cn/fc3679511e55f464/getVisitCount";
+      }else{
+         url = "http://cloud.bmob.cn/fc3679511e55f464/getCurrPV";
+      }
+      $.ajax({ 
+          url:url,  
+          type: "post",
+          contentType:"application/json; charset=utf-8",
+          dataType:'jsonp', 
+          data:'',
+          jsonp:'callback', 
+          success: function(result) {
+             pvcountObj.pvcount= result;
+          }      
+      });
+      var visitcount_vm = new Vue({
+         el:"#leftcircle",
+         data:pvcountObj
+      });
+       
+   }
+
+   
+
 })
